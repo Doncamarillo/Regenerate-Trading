868d56dd4ac1b35130a908ebb96019cce6aba1c5
doncamarillo <don_camarillo@yahoo.com>
2024-09-27 11:15:08 -0700

    populated html data from source API

--- script.js
+++ script.js
@@ -4,15 +4,40 @@ const tickerSymbol = document.querySelector('#tickerSymbol');
 const button = document.querySelector('#submitButton');
 
 
+const stockPrice = document.querySelector('#currentPrice');
+const priceChange = document.querySelector('#change');
+const percentChange = document.querySelector('#percentChange')
+const dailyHigh = document.querySelector('#dailyHigh');
+const dailyLow = document.querySelector('#openPrice');
+const openPrice = document.querySelector('#openPrice');
+const yesterdayClose = document.querySelector('#yesterdayClose');
+
+
 const getFinnhub = async (tickerSymbol) => {
 
-        const response = await axios.get(`https://finnhub.io/api/v1/quote?symbol=${tickerSymbol}&token=crrd0h9r01qmgcu5vq40crrd0h9r01qmgcu5vq4g`);
-        const stockData = response.data;
-        console.log('Finnhub API:', stockData);
+    const response = await axios.get(`https://finnhub.io/api/v1/quote?symbol=${tickerSymbol}&token=crrd0h9r01qmgcu5vq40crrd0h9r01qmgcu5vq4g`);
+    const stockData = response.data;
+    console.log('Finnhub API:', stockData);
+
+    let priceQuote = stockData.c
+    let changePrice = stockData.d
+    let changePercent = stockData.dp
+    let high = stockData.h
+    let low = stockData.l
+    let priceOpen =stockData.o
+    let lastClose = stockData.pc
+
+    stockPrice.textContent = `${priceQuote}`
+    priceChange.textContent = `${changePrice}`
+    percentChange.textContent =`${changePercent}`
+    dailyHigh.textContent =`${high}`
+    dailyLow.textContent =`${low}`
+    openPrice.textContent =`${priceOpen}`
+    yesterdayClose.textContent =`${lastClose}`
 
 }
 
 button.addEventListener('click', () => {
-    const ticker = tickerSymbol.value; // Correctly get the input value
+    const ticker = tickerSymbol.value.trim(); 
     getFinnhub(ticker);
 });
